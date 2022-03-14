a1b8f94dc1fa097cd4a230d2e61fb20cd9cd19dc
sidharthkumar2k19 <sidharthkumar2k19@gmail.com>
2022-03-14 20:19:17 +0530

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const {mongoose} = require('mongoose');
+const User = require('./models/User');
+const {Schema} = mongoose;
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,7 +12,9 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then( () => console.log('Connected to DB ...') )
+  .catch( error => console.log(error.message) );
 
 
 
